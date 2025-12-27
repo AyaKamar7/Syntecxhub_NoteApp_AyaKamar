7ef8a133f6907530f81a1ce51d626d2799775aca
AyaKamar7 <140603746+AyaKamar7@users.noreply.github.com>
2025-12-27 16:18:21 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Header from './Components/header'
+import Notes from './Components/notes'
+Notes
+
+function App() {
+  
+
+  return (
+    <div className='main'>
+      <Header />
+      <Notes/>
+
+
+    </div>
+ 
+  )
+}
+
+export default App
